c080eb09298df13eec1a0c7ffe54f7d8c75439d8
Surya <surya.s08080@gmail.com>
2025-05-25 17:13:35 +0530

    json files in post man curl

---
+++ src/main/java/json.java
@@ -0,0 +1,29 @@
+
+public class json {
+
+/*	curl --location 'http://localhost:8080/api/login/register' \
+	--header 'Content-Type: application/json' \
+	--data-raw '{
+	       "name": "Surya",
+	       "emailId": "surya@example.com",
+	       "mobileNumber": "9787799039",
+	       "password": "surya123"
+	     }'
+
+curl--location'http:// localhost:8080/api/createAccount' \
+--header'Content-Type:application/json' \--data-raw'{"name":"Surya","mobileNumber":"1234567890","emailId":"surya@example.com","password":"surya143","login":{"id":1}}'
+
+
+curl --location 'http://localhost:8080/api/getAll/account?Id=1'
+
+curl --location 'http://localhost:8080/api/credit' \
+--header 'Content-Type: application/json' \
+--data '{
+       "mobileNumber": "1234567890",
+       "amount": 2000.0
+     }'
+
+curl --location 'http://localhost:8080/api/checkBalance?mobileNumber=1234567890'
+
+*/
+}
